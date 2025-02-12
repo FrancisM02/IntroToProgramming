47b6cf02a02f062661c21fb19591935b18913809
Francis <105885275+FrancisM02@users.noreply.github.com>
2025-02-12 16:50:09 -0500

    Add files via upload
    (Area and perimeter of a circle) Write a program that displays the area and perimeter of a circle that has a radius of 5.5 using the following formulas:

---
+++ 1.8.py
@@ -0,0 +1,12 @@
+# Write a program that displays the area and perimeter of a circle that has a radius of 5.5
+# using the following formula
+# area = radius * radius * pi
+# perimeter = 2 * radius * pi
+
+radius = 5.5
+area = 3.14 * radius * radius
+perimeter = 2 * radius * 3.14
+
+print("The area of a circle with radius",radius, "is",area)
+print("The perimeter of a circle with area",area, "is",perimeter)
+
