0f0161c06d288bd58265ef923ca851e8c5b99d14
Francis <105885275+FrancisM02@users.noreply.github.com>
2025-02-12 16:53:09 -0500

    Add files via upload

---
+++ 1.9.py
@@ -0,0 +1,11 @@
+# (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of rectangle
+# with the width of 4.5 and height of 7.9 using the following formula
+# area = width * height
+
+width = 4.5
+height = 7.9
+area = width * height
+perimeter = 2 * (width + height)
+
+print(f"The area of the rectangle with a width of {width} and height of {height} is {area}")
+print(f"The perimeter of this rectangle is {perimeter}")
